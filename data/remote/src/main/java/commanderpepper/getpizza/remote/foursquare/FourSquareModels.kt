87feafe8eb47841f24e3e489cd66a0a4d3ff951d
humza <ahmadhumza7@gmail.com>
2024-10-17 15:44:40 -0700

    Added data classes for remote API

---
+++ data/remote/src/main/java/commanderpepper/getpizza/remote/foursquare/FourSquareModels.kt
@@ -0,0 +1,72 @@
+package commanderpepper.getpizza.remote.foursquare
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Category(
+    val id: String,
+    val name: String,
+    val pluralName: String,
+    val shortName: String,
+    val icon: Icon,
+    val primary: Boolean,
+    val venuePage: VenuePage
+)
+
+@Serializable
+data class Icon(
+    val prefix: String,
+    val suffix: String
+)
+
+@Serializable
+data class LabeledLatLngs(
+    val label: String,
+    val lat: Float,
+    val lng: Float
+)
+
+@Serializable
+data class Location(
+    val address: String,
+    val crossStreet: String,
+    val lat: Float,
+    val lng: Float,
+    val labeledLatLngs: List<LabeledLatLngs>,
+    val distance: Int,
+    val postalCode: String,
+    val cc: String,
+    val city: String,
+    val state: String,
+    val formattedAddress: List<String>
+)
+
+@Serializable
+data class Meta(
+    val code: Int,
+    val requestId: String
+)
+
+@Serializable
+data class Response(
+    val venues: List<Venue>,
+    val categories: List<Category>?
+)
+
+@Serializable
+data class SearchResponse(
+    val meta: Meta,
+    val response: Response
+)
+
+@Serializable
+data class Venue(
+    val id: String,
+    val name: String,
+    val location: Location
+)
+
+@Serializable
+data class VenuePage(
+    val id: String
+)
