a965f09f913b82f9eb021aa18f4734ab2b7929d7
humza <ahmadhumza7@gmail.com>
2024-11-26 16:08:22 -0800

    Modified the project to have the Scaffold envelop the entire app

--- app/src/main/java/commanderpepper/getpizza/LauncherActivity.kt
+++ app/src/main/java/commanderpepper/getpizza/LauncherActivity.kt
@@ -8,39 +8,60 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.permissions.PermissionsScreen
 import commanderpepper.getpizza.map.PizzaMapScreen
+import commanderpepper.getpizza.model.feature.map.PizzaMarkerUIState
 import commanderpepper.getpizza.model.ui.FavoritesDestination
 import commanderpepper.getpizza.model.ui.MapDestination
 import commanderpepper.getpizza.model.ui.PermissionsDestination
 
 
 class LauncherActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = PermissionsDestination){
-                composable<MapDestination> {
-                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+                TopAppBar(title = { Text("Get Pizza") }, actions = {
+                    IconButton(onClick = {
+                        navController.navigate(FavoritesDestination) {
+                            launchSingleTop = true
+                        }
+                    }) {
+                        Image(
+                            painter = painterResource(
+                                commanderpepper.getpizza.map.R.drawable.ic_favorite
+                            ), contentDescription = "Favorites"
+                        )
+                    }
+                })
+            }) { innerPadding ->
+                NavHost(
+                    modifier = Modifier.padding(innerPadding),
+                    navController = navController,
+                    startDestination = PermissionsDestination
+                ) {
+                    composable<MapDestination> {
                         PizzaMapScreen(
                             modifier = Modifier
-                                .fillMaxSize()
-                                .padding(innerPadding),
+                                .fillMaxSize(),
                             onMapIconClick = { pizzaMarkerUIState ->
-                                val format =
-                                    pizzaMarkerUIState.address?.let { "geo:0,0?q=" + it + "(" + attr.label + ")" }
-                                        ?: ("geo:0,0?q=" + pizzaMarkerUIState.lat.toString() + "," + pizzaMarkerUIState.lng.toString() + "(" + attr.label + ")")
-                                val gmmIntentUri = Uri.parse(format)
-                                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                                val mapIntent = createMapIntent(pizzaMarkerUIState)
                                 mapIntent.setPackage("com.google.android.apps.maps")
                                 startActivity(mapIntent)
                             },
@@ -50,24 +71,38 @@ class LauncherActivity : ComponentActivity() {
                                 }
                                 startActivity(intent)
                             })
-                    }
-                }
-                composable<FavoritesDestination> {
 
-                }
-                composable<PermissionsDestination> {
-                    PermissionsScreen(
-                        onDismiss = { lat, lng ->
-                            val mapDestination = MapDestination(lat, lng)
-                            navController.navigate(mapDestination)
-                        },
-                        onPermissionGranted = { lat, lng ->
-                            val mapDestination = MapDestination(lat, lng)
-                            navController.navigate(mapDestination)
-                        }
-                    )
+                    }
+                    composable<FavoritesDestination> {
+
+                    }
+                    composable<PermissionsDestination> {
+                        PermissionsScreen(
+                            onDismiss = { lat, lng ->
+                                val mapDestination = MapDestination(lat, lng)
+                                navController.navigate(mapDestination) {
+                                    launchSingleTop = true
+                                }
+                            },
+                            onPermissionGranted = { lat, lng ->
+                                val mapDestination = MapDestination(lat, lng)
+                                navController.navigate(mapDestination) {
+                                    launchSingleTop = true
+                                }
+                            }
+                        )
+                    }
                 }
             }
         }
+    }
+
+    private fun createMapIntent(pizzaMarkerUIState: PizzaMarkerUIState): Intent {
+        val format =
+            pizzaMarkerUIState.address?.let { "geo:0,0?q=" + it + "(" + attr.label + ")" }
+                ?: ("geo:0,0?q=" + pizzaMarkerUIState.lat.toString() + "," + pizzaMarkerUIState.lng.toString() + "(" + attr.label + ")")
+        val gmmIntentUri = Uri.parse(format)
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        return mapIntent
     }
 }
