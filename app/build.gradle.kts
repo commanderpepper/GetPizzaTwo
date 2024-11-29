85d71199bdd447a83dd20da472eb69f829351854
humza <ahmadhumza7@gmail.com>
2024-11-28 17:57:46 -0800

    Updated version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 10
-        versionName = "2.0"
+        versionName = "3.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
